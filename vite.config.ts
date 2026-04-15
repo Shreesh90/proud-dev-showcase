65dbefd4c4ef67e9514d3d919b854f61647cee86
Shreesh <chandra@Shreeshs-MacBook-Air.local>
2026-04-16 00:37:09 +0530

    changed url to portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/proud-dev-showcase/",
+  base: "/portfolio/",
   server: {
     host: "::",
     port: 8080,
